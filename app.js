4b391fb754a559e40fac82813a2a275c44fc8269
sysmoon <sysmoon@gmail.com>
2022-11-09 10:34:13 +0900

    미들웨어를 이용한 다른 라우팅 방법

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
+app.use("/", (req, res, next) => {
+  console.log("this is alwqays run!");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("in the middleware2");
-  res.send("it's sunny day!");
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1> Add Product </h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in the middleware");
+  res.send("<h1> Hello Express </h1>");
 });
 
 app.listen(3000);
