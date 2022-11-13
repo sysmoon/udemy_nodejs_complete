fe1305934236404db3c157b3229beb597ca5b78c
sysmoon <sysmoon@gmail.com>
2022-11-13 22:29:20 +0900

    67. 404 오류페이지 추가

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1> Page not Found </h1>");
+});
+
 // run server
 app.listen(3000);
