16ece6344ffcd630ee3ca16a461c74e58c51928e
sysmoon <sysmoon@gmail.com>
2022-11-13 23:54:13 +0900

    73. 네비게이션을 위한 헬퍼 함수 사용

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const rootDir = require("./util/path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -15,7 +16,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 // run server
