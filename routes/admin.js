f6cf60c3bec6fa82e92f0f92a9c79bcdb891e95c
sysmoon <sysmoon@gmail.com>
2022-11-13 23:10:33 +0900

    70. html 페이지 서비스하기

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,14 @@
+const path = require("path");
 const express = require("express");
-
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST"> <input type="text" name="title"> <button type="submit"> Add Product </button>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
